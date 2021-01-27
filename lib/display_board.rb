62917f049fc7ba3ec41aeb4300a00ac5c861f287
Reid Wagner <rcwagner10@gmail.com>
2021-01-27 00:09:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,5 +7,5 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
-display_board()
+display_board(board)
 board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
